e254ec307265da61a2f8d76cadc40b8d1e7f2f1a
[akshephard] <akshephard@ucdavis.edu>
2019-03-25 15:12:55 -0700

    added make file back in

---
+++ Makefile
@@ -0,0 +1,24 @@
+GOPATH = /home/gabe/go
+PLUGINS=$(wildcard plugins/*.go)
+
+.PHONY: proto
+proto: proto/xbos.proto
+	protoc -Iproto/ -Iproto/googleapis --go_out=plugins=grpc:proto proto/*.proto
+
+.PHONY: proto-py
+proto-py: wavemq/mqpb/wavemq.proto
+	mkdir -p python/pyxbos/pyxbos/wavemq
+	mkdir -p python/pyxbos/pyxbos/wave
+	cp wavemq/mqpb/*.proto python/pyxbos/pyxbos/wavemq
+	cp wave/eapi/pb/*.proto python/pyxbos/pyxbos/wave
+	cd python/pyxbos; \
+	poetry run python3 -m grpc_tools.protoc -Ipyxbos/wavemq -I../../proto/googleapis --python_out=pyxbos --grpc_python_out=pyxbos pyxbos/wavemq/*.proto; \
+	poetry run python3 -m grpc_tools.protoc -Ipyxbos/wave -I../../proto/googleapis --python_out=pyxbos/wave --grpc_python_out=pyxbos/wave pyxbos/wave/*.proto; \
+	poetry run python3 -m grpc_tools.protoc -I../../proto -I../../proto/googleapis --python_out=pyxbos ../../proto/*.proto; \
+	sed -i -e 's/^import \(.*_pb2\)/from . import \1/g' pyxbos/*pb2*.py; \
+	sed -i -e 's/^import \(.*_pb2\)/from . import \1/g' pyxbos/wave/*pb2*.py
+
+#venv: python/requirements.txt
+#	python3 -m venv venv; \
+#	. venv/bin/activate; \
+#	pip3 install -r python/requirements.txt
--- proto/nullabletypes.pb.go
+++ proto/nullabletypes.pb.go
@@ -3,11 +3,9 @@
 
 package xbospb
 
-import (
-	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
-	math "math"
-)
+import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -31,7 +29,7 @@ func (m *Int32) Reset()         { *m = Int32{} }
 func (m *Int32) String() string { return proto.CompactTextString(m) }
 func (*Int32) ProtoMessage()    {}
 func (*Int32) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a606d93d67bf0130, []int{0}
+	return fileDescriptor_nullabletypes_ffbf8acdca2a5424, []int{0}
 }
 func (m *Int32) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Int32.Unmarshal(m, b)
@@ -39,8 +37,8 @@ func (m *Int32) XXX_Unmarshal(b []byte) error {
 func (m *Int32) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Int32.Marshal(b, m, deterministic)
 }
-func (m *Int32) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Int32.Merge(m, src)
+func (dst *Int32) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Int32.Merge(dst, src)
 }
 func (m *Int32) XXX_Size() int {
 	return xxx_messageInfo_Int32.Size(m)
@@ -69,7 +67,7 @@ func (m *Int64) Reset()         { *m = Int64{} }
 func (m *Int64) String() string { return proto.CompactTextString(m) }
 func (*Int64) ProtoMessage()    {}
 func (*Int64) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a606d93d67bf0130, []int{1}
+	return fileDescriptor_nullabletypes_ffbf8acdca2a5424, []int{1}
 }
 func (m *Int64) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Int64.Unmarshal(m, b)
@@ -77,8 +75,8 @@ func (m *Int64) XXX_Unmarshal(b []byte) error {
 func (m *Int64) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Int64.Marshal(b, m, deterministic)
 }
-func (m *Int64) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Int64.Merge(m, src)
+func (dst *Int64) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Int64.Merge(dst, src)
 }
 func (m *Int64) XXX_Size() int {
 	return xxx_messageInfo_Int64.Size(m)
@@ -107,7 +105,7 @@ func (m *Uint64) Reset()         { *m = Uint64{} }
 func (m *Uint64) String() string { return proto.CompactTextString(m) }
 func (*Uint64) ProtoMessage()    {}
 func (*Uint64) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a606d93d67bf0130, []int{2}
+	return fileDescriptor_nullabletypes_ffbf8acdca2a5424, []int{2}
 }
 func (m *Uint64) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Uint64.Unmarshal(m, b)
@@ -115,8 +113,8 @@ func (m *Uint64) XXX_Unmarshal(b []byte) error {
 func (m *Uint64) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Uint64.Marshal(b, m, deterministic)
 }
-func (m *Uint64) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Uint64.Merge(m, src)
+func (dst *Uint64) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Uint64.Merge(dst, src)
 }
 func (m *Uint64) XXX_Size() int {
 	return xxx_messageInfo_Uint64.Size(m)
@@ -145,7 +143,7 @@ func (m *Double) Reset()         { *m = Double{} }
 func (m *Double) String() string { return proto.CompactTextString(m) }
 func (*Double) ProtoMessage()    {}
 func (*Double) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a606d93d67bf0130, []int{3}
+	return fileDescriptor_nullabletypes_ffbf8acdca2a5424, []int{3}
 }
 func (m *Double) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Double.Unmarshal(m, b)
@@ -153,8 +151,8 @@ func (m *Double) XXX_Unmarshal(b []byte) error {
 func (m *Double) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Double.Marshal(b, m, deterministic)
 }
-func (m *Double) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Double.Merge(m, src)
+func (dst *Double) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Double.Merge(dst, src)
 }
 func (m *Double) XXX_Size() int {
 	return xxx_messageInfo_Double.Size(m)
@@ -183,7 +181,7 @@ func (m *Bool) Reset()         { *m = Bool{} }
 func (m *Bool) String() string { return proto.CompactTextString(m) }
 func (*Bool) ProtoMessage()    {}
 func (*Bool) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a606d93d67bf0130, []int{4}
+	return fileDescriptor_nullabletypes_ffbf8acdca2a5424, []int{4}
 }
 func (m *Bool) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Bool.Unmarshal(m, b)
@@ -191,8 +189,8 @@ func (m *Bool) XXX_Unmarshal(b []byte) error {
 func (m *Bool) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Bool.Marshal(b, m, deterministic)
 }
-func (m *Bool) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Bool.Merge(m, src)
+func (dst *Bool) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Bool.Merge(dst, src)
 }
 func (m *Bool) XXX_Size() int {
 	return xxx_messageInfo_Bool.Size(m)
@@ -218,16 +216,16 @@ func init() {
 	proto.RegisterType((*Bool)(nil), "xbospb.Bool")
 }
 
-func init() { proto.RegisterFile("nullabletypes.proto", fileDescriptor_a606d93d67bf0130) }
+func init() { proto.RegisterFile("nullabletypes.proto", fileDescriptor_nullabletypes_ffbf8acdca2a5424) }
 
-var fileDescriptor_a606d93d67bf0130 = []byte{
-	// 127 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xce, 0x2b, 0xcd, 0xc9,
+var fileDescriptor_nullabletypes_ffbf8acdca2a5424 = []byte{
+	// 126 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0x12, 0xce, 0x2b, 0xcd, 0xc9,
 	0x49, 0x4c, 0xca, 0x49, 0x2d, 0xa9, 0x2c, 0x48, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
 	0x62, 0xab, 0x48, 0xca, 0x2f, 0x2e, 0x48, 0x52, 0x92, 0xe5, 0x62, 0xf5, 0xcc, 0x2b, 0x31, 0x36,
 	0x12, 0x12, 0xe1, 0x62, 0x2d, 0x4b, 0xcc, 0x29, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x0d,
 	0x82, 0x70, 0xa0, 0xd2, 0x66, 0x26, 0xa8, 0xd2, 0xcc, 0x30, 0x69, 0x39, 0x2e, 0xb6, 0xd0, 0x4c,
-	0x4c, 0x79, 0x16, 0x24, 0x79, 0x97, 0xfc, 0xd2, 0xa4, 0x9c, 0x54, 0x54, 0x79, 0x46, 0x98, 0xbc,
-	0x0c, 0x17, 0x8b, 0x53, 0x7e, 0x7e, 0x0e, 0xaa, 0x2c, 0x07, 0x54, 0x36, 0x89, 0x0d, 0xec, 0x54,
-	0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x76, 0x2e, 0x5c, 0xff, 0xc1, 0x00, 0x00, 0x00,
+	0x4c, 0x79, 0x16, 0x24, 0x79, 0x97, 0xfc, 0x52, 0xa0, 0xd5, 0xa8, 0xf2, 0x8c, 0x30, 0x79, 0x19,
+	0x2e, 0x16, 0xa7, 0xfc, 0xfc, 0x1c, 0x54, 0x59, 0x0e, 0xa8, 0x6c, 0x12, 0x1b, 0xd8, 0xa9, 0xc6,
+	0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x76, 0x2e, 0x5c, 0xff, 0xc1, 0x00, 0x00, 0x00,
 }
