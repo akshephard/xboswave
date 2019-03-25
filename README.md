cd2fc12fc8ed20fea8b5f25ac1150ddd9557d7b2
[akshephard] <akshephard@ucdavis.edu>
2019-03-25 11:00:30 -0700

    added all of the directories back

---
+++ README.md
@@ -0,0 +1,36 @@
+# XBOS(2) 
+
+- [Issues](https://todo.sr.ht/%7Egabe/xbos)
+- [Docs](https://man.sr.ht/%7Egabe/xbos/)
+
+## Ingester
+
+Requirements:
+1. go: https://golang.org/doc/install
+2. bzr
+    ```
+    sudo apt-get install bzr
+    ```
+
+How do get started:
+
+
+1. Install [BtrDB dev machine](https://docs.smartgrid.store/development-environment.html) OR [InfluxDB](https://docs.influxdata.com/influxdb/v1.7/introduction/)
+2. Install [WAVEMQ](https://github.com/immesys/wavemq):
+    - can use ansible:
+    1. [Install ansible](https://docs.ansible.com/ansible/2.7/installation_guide/intro_installation.html#installing-the-control-machine)
+    2. Clone this repo
+    3. Go to `ansible/`
+    4. Run the playbook:
+        ```
+        ansible-playbook -K wavemq-playbook.yml
+        ```
+3. Create entity (docs coming)
+    - read [wave docs](https://github.com/immesys/wave) for now
+4. Give perms
+    - read [wave docs](https://github.com/immesys/wave) for now
+5. Build and run ingester:
+    ```
+    cd ingester
+    make run
+    ```
---
+++ tools/wave.sh
@@ -0,0 +1,186 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+ProgName=$(basename $0)
+#XBOS_NAMESPACE=
+WAVE_DEFAULT_ENTITY=/home/gabe/.wave/gabe.wave.ent
+
+sub_help(){
+    echo "Usage: $ProgName <subcommand> [options]\n"
+    echo "Subcommands:"
+    echo "    mk_namespace  <namespace name>"
+    echo "    check_ns_access <namespace name> <entity path?>"
+    echo "    mk_driver_ent <driver name> <namespace name>"
+    echo "    grant_dr <namespace name> <router entity>"
+    echo ""
+    echo "For help with each subcommand run:"
+    echo "$ProgName <subcommand> -h|--help"
+    echo ""
+}
+
+sub_mk_namespace(){
+    echo "Making namespace entity"
+    # checks if the name argument is there
+    if [ -z "$1" ]; then
+        echo "Need to provide a name for the entity"
+        exit 1;
+    fi
+    name=$1
+    if [ ! -f $name.ent ]; then
+        printf "${YELLOW}Creating namespace${NC}\n"
+        wv mke -e 50y --nopassphrase -o $name.ent
+        echo '...skipping passphrase...'
+    else
+        printf "${YELLOW}Already exists${NC}\n"
+    fi
+    echo '\n' | wv name --public --attester $WAVE_DEFAULT_ENTITY $name.ent $name
+    echo '\n' | wv name --public --attester $name.ent $name.ent $name
+    #echo '\n' | wv rtprove -o proof.pem --subject $WAVE_DEFAULT_ENTITY wavemq:publish,subscribe@$name/*
+    sub_check_ns_access $name
+    if [ $success -ne 0 ]; then
+        printf "${YELLOW}No existing access; granting to ${WAVE_DEFAULT_ENTITY} ${NC}\n"
+        echo '\n' | wv rtgrant --subject $WAVE_DEFAULT_ENTITY -e 3y --attester $name.ent --indirections 5 wavemq:publish,subscribe@$name/*
+        sub_check_ns_access $name
+    fi
+}
+
+sub_check_ns_access(){
+    if [ -z "$1" ]; then
+        echo "Need to provide a namespace"
+        exit 1;
+    fi
+
+    ns=$1
+
+    if [ -z "$2" ]; then
+        printf "Check access for default ent\n"
+        target=$WAVE_DEFAULT_ENTITY
+    else
+        printf "Check access for ${2}\n"
+        target=$2
+    fi
+
+    if [ -z "$3" ];then
+        resource="*"
+    else
+        resource=$3
+    fi
+
+    echo "Check access to $ns for $target"
+
+    echo '\n' | wv rtprove --subject $target -o proof.pem wavemq:publish,subscribe@$ns/$resource
+    if [ $? -ne 0 ]; then
+        printf "${RED}-- no access --${NC}\n"
+        success=1
+    else
+        wv verify proof.pem
+        if [ $? -eq 0 ]; then
+            printf "${GREEN}-- ok --${NC}\n"
+            success=0
+        else
+            printf "${RED}-- no access --${NC}\n"
+            success=1
+        fi
+    fi
+}
+
+sub_mk_driver_ent(){
+    echo "Making driver entity"
+    # checks if the name argument is there
+    if [ -z "$1" ]; then
+        echo "Need to provide a name for the entity"
+        exit 1;
+    fi
+    if [ -z "$2" ]; then
+        echo "Need to provide a name for the namespace"
+        exit 1;
+    fi
+    if [ -z "$3" ];then
+        resource="$name/*"
+    else
+        resource=$3
+    fi
+
+    name=$1
+    ns=$2
+
+    if [ ! -f $name.ent ]; then
+        printf "${YELLOW}Entity does not exist; creating${NC}\n"
+        wv mke -e 10y --nopassphrase -o $name.ent
+        echo '...skipping passphrase...'
+    else
+        printf "${YELLOW}Already exists${NC}\n"
+    fi
+    echo '\n' | wv name --public --attester $WAVE_DEFAULT_ENTITY $name.ent $name
+    echo '\n' | wv name --public --attester $name.ent $ns.ent $ns
+
+    sub_check_ns_access $ns $name.ent $resource
+    if [ $success -ne 0 ]; then
+        printf "${YELLOW}No existing access; granting to ${name} ${NC}\n"
+        echo '\n' | wv rtgrant --subject $name -e 3y --attester $WAVE_DEFAULT_ENTITY --indirections 0 wavemq:publish,subscribe@$ns/$resource
+        sub_check_ns_access $ns $name.ent $resource
+    else
+        printf "${GREEN}Already has access${NC}\n"
+    fi
+}
+
+sub_grant_dr(){
+    # input:
+    # - namespace name
+    # - PK of router
+    # checks if the name argument is there
+    if [ -z "$1" ]; then
+        echo "Need to provide a namespace entity to grant routing permissions"
+        exit 1;
+    fi
+    if [ -z "$2" ]; then
+        echo "Need to provide a path to the designated router entity"
+        exit 1;
+    fi
+
+    nsent=$1
+    ns="${nsent%.*}"
+    routerent=$2
+    echo '\n' | wv rtprove -o routerproof.pem --subject $routerent wavemq:route@$nsent/*
+    if [ $? -eq 0 ]; then
+        wv verify routerproof.pem
+        if [ $? -eq 0 ]; then
+            cp routerproof.pem $ns-routerproof.pem
+            printf "${GREEN}-- ok --${NC}\n"
+            success=0
+            return
+        fi
+    fi
+    printf "${YELLOW}-- no access. granting... --${NC}\n"
+    echo '\n' | wv rtgrant --subject $routerent -e 3y --attester $nsent --indirections 0 wavemq:route@$nsent/*
+    echo '\n' | wv rtprove -o routerproof.pem --subject $routerent wavemq:route@$nsent/*
+    wv verify routerproof.pem
+    if [ $? -eq 0 ]; then
+        printf "${GREEN}-- ok --${NC}\n"
+        success=0
+    else
+        printf "${RED}-- no access --${NC}\n"
+        success=1
+    fi
+    cp routerproof.pem $ns-routerproof.pem
+}
+
+subcommand=$1
+case $subcommand in
+    "" | "-h" | "--help")
+        sub_help
+        ;;
+    *)
+        shift
+        sub_${subcommand} $@
+        if [ $? = 127 ]; then
+            echo "Error: '$subcommand' is not a known subcommand." >&2
+            echo "       Run '$ProgName --help' for a list of known subcommands." >&2
+            exit 1
+        fi
+        ;;
+esac
